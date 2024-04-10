91b11056c9199eb211cbfd39cc44b097beb6c0c0
Tadachini013 <163538734+Tadachini013@users.noreply.github.com>
2024-04-10 22:02:28 +0900

    File update
    File upate

--- script_2-1.py
+++ script_2-1.py
@@ -1,16 +1,29 @@
 money = 1000
+print("現在の所持金"+str(money)+"円")
 items = {'apple': 100, 'banana': 200, 'orange': 400}
+Firstbuy = True
+
+print("本日の売り出しもの")
+for dis_mess in items:
+    print(dis_mess + '：1個' + str(items[dis_mess]) + '円')
+      
 for item_name in items:
-    print('--------------------------------------------------')
-    print('財布には' + str(money) + '円入っています')
-    print(item_name + 'は1個' + str(items[item_name]) + '円です')
+    print('-----------------ご注文---------------------------')
     
     input_count = input('購入する' + item_name + 'の個数を入力してください：')
-    print('購入する' + item_name + 'の個数は' + input_count + '個です')
-    
+    #print('購入する' + item_name + 'の個数は' + input_count + '個です')
+    print(item_name+":"+input_count+"個")
     count = int(input_count)
     total_price = items[item_name] * count
-    print('支払い金額は' + str(total_price) + '円です')
+    
+    #そもそもお金が足りなかった場合
+    if((total_price > money) & Firstbuy):
+        print("お金が足りません。買い物を中断します")
+        break 
+    #少なくとも買い物はできる
+    Firstbuy=False
+
+    print('支払い金額：' + str(total_price) + '円')
     
     if money >= total_price:
         print(item_name + 'を' + input_count + '個買いました')
@@ -22,4 +35,5 @@ for item_name in items:
     else:
         print('お金が足りません')
         print(item_name + 'を買えませんでした')
+        
 print('残金は' + str(money) + '円です')
